5baa67bc2cf5a3e4a446743a17f6098a511caa6e
Ariella Falcao <83048088+AriellaFalcao@users.noreply.github.com>
2021-08-30 15:40:18 -0300

    Exibicao dos dados contatos na tela

--- src/app/components/lista-de-contatos/lista-de-contatos.component.ts
+++ src/app/components/lista-de-contatos/lista-de-contatos.component.ts
@@ -10,27 +10,27 @@ export class ListaDeContatosComponent implements OnInit {
 
   contatos: Contato[] = [
     {
-      nome:"Duda",
-      email:"duda@gmail.com",
-      telefone:["77777-7777"]
+      nome:"Duda 1",
+      email:"duda1@gmail.com",
+      telefone:["11111-1111"]
 
     },
     {
-      nome:"Duda",
-      email:"duda@gmail.com",
-      telefone:["77777-7777"]
+      nome:"Duda 2",
+      email:"duda2@gmail.com",
+      telefone:["22222-2222"]
 
     },
     {
-      nome:"Duda",
-      email:"duda@gmail.com",
-      telefone:["77777-7777"]
+      nome:"Duda 3",
+      email:"duda3@gmail.com",
+      telefone:["3333-3333"]
 
     },
     {
-      nome:"Duda",
-      email:"duda@gmail.com",
-      telefone:["77777-7777"]
+      nome:"Duda 4",
+      email:"duda4@gmail.com",
+      telefone:["44444-4444"]
 
     }
 
